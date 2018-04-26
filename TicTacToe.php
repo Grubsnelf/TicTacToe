4aadcd21610b590bd31a3f8bcf98203742957519
Tim Stolze <tim.stolze@gmx.de>
2018-04-26 10:02:44 +0200

    edited currentStatus() first simple test for winner

--- TicTacToe.php
+++ TicTacToe.php
@@ -28,6 +28,7 @@ class TicTacToe
 	public function __construct ($player1, $player2, $board){
 		$this->player1 = $player1;
 		$this->player2 = $player2;
+		$this->currentPlayer = $this->player1;
 		$this->board = $board;
 	}
 	
@@ -48,13 +49,23 @@ class TicTacToe
 	*/
 	public function move(){
 		$this->board->placeSymbol();
+		$this->currentStatus();
 	}
 	
 	/**
 	*
 	*/
 	public function currentStatus(){
-
+		$symbol = $this->currentPlayer->getSymbol();
+		$zaehler = 0;
+		for($col = 0; $col <= 2; $col++){
+			for($row = 0; $row <= 2; $row++){
+				if($board[$col][row] !== $symbol){
+					$zaehler += 1;
+				}	
+			}
+		}
+		print_r($zaehler);
 	}
 	
 }
