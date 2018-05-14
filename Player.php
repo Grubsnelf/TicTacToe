2fb7084656e7a8133f8a8fa8f2e5845494d7d71f
Tim Stolze <tim.stolze@gmx.de>
2018-05-14 09:19:30 +0200

    Added comments

--- Player.php
+++ Player.php
@@ -3,27 +3,34 @@
 class Player
 {
 	/**
-	*
+	* @var string $name the name
 	*/
 	private $name;
 	/**
-	*
+	* @var string $symbol the symbol
 	*/
 	private $symbol;
 	
 	/**
-	*
+	* Sets $name / $symbol
+	* @param $name as string
+	* @param $symbol as string
 	*/
 	public function __construct ($name, $symbol){
 		$this->name = $name;
 		$this->symbol = $symbol;
 	}
 	
+	/**
+	* Returns the name
+	*/
 	public function getName(){
 		return($this->name);
 	}
 	
-	
+	/**
+	* Returns the symbol
+	*/
 	public function getSymbol(){
 		return($this->symbol);
 	}
